0d80a531ee8fc3c2651b5273a3c32cc39c0090de
Esa Juhani Ruoho <esaruoho@gmail.com>
2015-12-28 18:06:10 +0200

    tidying up (replace random var names with good 1s)

--- OneBorkedTwoToGo/ViewController.swift
+++ OneBorkedTwoToGo/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  OneBorkedTwoToGo
+//  RGBSliderSepia
 //
 //  Created by Esa Ruoho on 28/12/15.
 //  Copyright © 2015 Esa Ruoho. All rights reserved.
@@ -15,48 +15,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var redSlider: UISlider!
-    
     @IBOutlet weak var labelLabel: UILabel!
-    
     @IBOutlet weak var imageViewer: UIImageView!
-    
     @IBAction func sliderValueChanged(sender: AnyObject) {
-        let hohoo=String(redSlider.value)
-        let hohoo2=String(greenSlider.value)
-        let hohoo3=String(blueSlider.value)
-        
-        
+        let redInfo=String(redSlider.value)
+        let greenInfo=String(greenSlider.value)
+        let blueInfo=String(blueSlider.value)
         
         self.view.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
         
-        let result = "R: " + hohoo + " G: " + hohoo2 + " B: " + hohoo3
+        let result = "R: " + redInfo + " G: " + greenInfo + " B: " + blueInfo
         labelLabel.text = result
-        
         labelLabel.backgroundColor = UIColor.whiteColor()
-        
-//        self.imageViewer.backgroundColor =
-        
     }
     
-    
     @IBAction func redSliderTouchUp(sender: AnyObject) {
-
-
-        
-
+    // decided that this needs to do nothing.
     }
     
     @IBOutlet weak var sliderValue: UISlider!
-    
     @IBAction func Slider(sender: AnyObject) {
-        
-        
+        // From Ray Wenderlich's tutorial http://www.raywenderlich.com/76285/beginning-core-image-swift
         filter.setValue(beginImage, forKey: kCIInputImageKey)
         filter.setValue(CGFloat(sliderValue.value), forKey: kCIInputIntensityKey)
         let newImage = UIImage(CIImage: filter.outputImage!)
         self.imageViewer.image = newImage
-        
-        
     }
     
     override func viewDidLoad() {
@@ -64,23 +47,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         redSlider.value = 1.0
         
-        let hohoo=String(redSlider.value)
-        let hohoo2=String(greenSlider.value)
-        let hohoo3=String(blueSlider.value)
-        
-        
-        let result = "R: " + hohoo + " G: " + hohoo2 + " B: " + hohoo3
-        
+        let redInfo=String(redSlider.value)
+        let greenInfo=String(greenSlider.value)
+        let blueInfo=String(blueSlider.value)
+       
+        let result = "R: " + redInfo + " G: " + greenInfo + " B: " + blueInfo
         self.view.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
         labelLabel.text = result
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
